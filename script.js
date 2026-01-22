2237ab718226aa18d91eee88310a16d8b770546c
Sumit Maurya <sumitmaurya746@gmail.com>
2026-01-22 15:21:30 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const apiKey = "4a67c178cf2eea10df8df32c515bde52";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=lucknow";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
 
 async function checkWeather() {
-    const response= await fetch(apiUrl+~`&appid=${apiKey}`);
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await response.json(); /*now data will contain all the info about weather*/
+    console.log(data);
 }
+checkWeather();
